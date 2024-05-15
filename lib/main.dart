090eb52fa92304784e155d206de1bb688a0d8c88
ciptadii <ciptadi117@gmail.com>
2024-05-15 15:36:22 +0700

    add image and video player list, add on tap for play/pause next/previous content, and add animated bar widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:instagram_stories_app/data.dart';
+import 'package:instagram_stories_app/models/story_model.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +15,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Instagram Stories',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,92 +30,258 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: StoryScreen(stories: stories),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class StoryScreen extends StatefulWidget {
+  const StoryScreen({
+    super.key,
+    required this.stories,
+  });
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  final List<Story> stories;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StoryScreen> createState() => _StoryScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _StoryScreenState extends State<StoryScreen>
+    with SingleTickerProviderStateMixin {
+  late PageController _pageController;
+  late AnimationController _animationController;
+  late VideoPlayerController _videoController;
+  int _currentIndex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    // TODO: implement initState
+    _pageController = PageController();
+    _animationController = AnimationController(vsync: this);
+    _videoController =
+        VideoPlayerController.networkUrl(Uri.parse(widget.stories[2].url))
+          ..initialize().then((value) => setState(() {}));
+    _videoController.play();
+
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _animationController.stop();
+        _animationController.reset();
+        setState(() {
+          if (_currentIndex + 1 < widget.stories.length) {
+            _currentIndex += 1;
+            _loadStory(story: widget.stories[_currentIndex]);
+          } else {
+            // Out of bounds - loop story
+            // You can also Navigator.of(context).pop() here
+            _currentIndex = 0;
+            _loadStory(story: widget.stories[_currentIndex]);
+          }
+        });
+      }
     });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    _animationController.dispose();
+    _videoController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final Story story = widget.stories[_currentIndex];
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: Colors.black,
+      body: GestureDetector(
+        onTapDown: (details) => _onTapDown(details, story),
+        child: Stack(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            PageView.builder(
+              controller: _pageController,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: widget.stories.length,
+              itemBuilder: (context, i) {
+                final Story story = widget.stories[i];
+                switch (story.media) {
+                  case MediaType.image:
+                    return CachedNetworkImage(
+                      imageUrl: story.url,
+                      fit: BoxFit.cover,
+                    );
+                  case MediaType.video:
+                    if (_videoController != null &&
+                        _videoController.value.isInitialized) {
+                      return FittedBox(
+                        fit: BoxFit.cover,
+                        child: SizedBox(
+                          width: _videoController.value.size.width,
+                          height: _videoController.value.size.height,
+                          child: VideoPlayer(_videoController),
+                        ),
+                      );
+                    }
+                }
+                return const SizedBox.shrink();
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Positioned(
+              top: 40,
+              left: 10,
+              right: 10,
+              child: Row(
+                children: widget.stories
+                    .asMap()
+                    .map((key, value) {
+                      return MapEntry(
+                        key,
+                        AnimatedBar(
+                          animationController: _animationController,
+                          position: key,
+                          currentIndex: _currentIndex,
+                        ),
+                      );
+                    })
+                    .values
+                    .toList(),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _onTapDown(TapDownDetails details, Story story) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double dx = details.globalPosition.dx;
+
+    if (dx < screenWidth / 3) {
+      setState(() {
+        if (_currentIndex - 1 >= 0) {
+          _currentIndex -= 1;
+          _loadStory(story: widget.stories[_currentIndex]);
+        }
+      });
+    } else if (dx > 2 * screenWidth / 3) {
+      setState(() {
+        if (_currentIndex + 1 < widget.stories.length) {
+          _currentIndex += 1;
+          _loadStory(story: widget.stories[_currentIndex]);
+        } else {
+          // Out of bounds - loop story
+          // You can also Navigator.of(context).pop() here
+          _currentIndex = 0;
+          _loadStory(story: widget.stories[_currentIndex]);
+        }
+      });
+    } else {
+      if (story.media == MediaType.video) {
+        if (_videoController.value.isPlaying) {
+          _videoController.pause();
+          _animationController.stop();
+        } else {
+          _videoController.play();
+          _animationController.forward();
+        }
+      }
+    }
+  }
+
+  void _loadStory({required Story story, bool animateToPage = true}) {
+    _animationController.stop();
+    _animationController.reset();
+
+    switch (story.media) {
+      case MediaType.image:
+        _animationController.duration = story.duration;
+        _animationController.forward();
+        break;
+      case MediaType.video:
+        // _videoController = null;
+        _videoController.dispose();
+        _videoController =
+            VideoPlayerController.networkUrl(Uri.parse(story.url))
+              ..initialize().then((value) {
+                setState(() {});
+                _videoController.play();
+                _animationController.forward();
+              });
+        break;
+    }
+
+    if (animateToPage) {
+      _pageController.animateToPage(
+        _currentIndex,
+        duration: const Duration(milliseconds: 1),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+}
+
+class AnimatedBar extends StatelessWidget {
+  const AnimatedBar({
+    required this.animationController,
+    required this.position,
+    required this.currentIndex,
+    Key? key,
+  }) : super(key: key);
+
+  final AnimationController animationController;
+  final int position;
+  final int currentIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 1.5),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return Stack(
+              children: <Widget>[
+                _buildContainer(
+                  double.infinity,
+                  position < currentIndex
+                      ? Colors.white
+                      : Colors.white.withOpacity(0.5),
+                ),
+                position == currentIndex
+                    ? AnimatedBuilder(
+                        animation: animationController,
+                        builder: (context, child) {
+                          return _buildContainer(
+                            constraints.maxWidth * animationController.value,
+                            Colors.white,
+                          );
+                        },
+                      )
+                    : const SizedBox.shrink(),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Container _buildContainer(double width, Color color) {
+    return Container(
+      height: 5.0,
+      width: width,
+      decoration: BoxDecoration(
+        color: color,
+        border: Border.all(
+          color: Colors.black26,
+          width: 0.8,
+        ),
+        borderRadius: BorderRadius.circular(3.0),
+      ),
     );
   }
 }
